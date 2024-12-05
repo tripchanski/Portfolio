e0004f449e2bf9ea8a2f7d9450203d03ba507985
Tripchanski <arturtripking01@gmail.com>
2024-12-06 00:23:56 +0300

    first_step

---
+++ script.js
@@ -0,0 +1,172 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const buttons = document.querySelectorAll(".btn");
+  const frames = document.querySelectorAll(".frame");
+  const cards = document.querySelectorAll('.card');
+
+
+  const welcomeText1 = document.querySelector("#welcome-text-1");
+  const welcomeText2 = document.querySelector("#welcome-text-2");
+
+  let isTypingAboutMe = true;
+
+
+  // circle ____________________________________________
+
+  const circle = document.createElement("div");
+
+  circle.id = "circle";
+  circle.style.position = "fixed";
+  circle.style.width = "1rem";
+  circle.style.height = "1rem";
+  circle.style.backgroundColor = "#333333";
+  circle.style.borderRadius = "50%";
+  circle.style.pointerEvents = "none";
+  circle.style.zIndex = "1000";
+  circle.style.transform = "translate(-50%, -50%)";
+
+
+  document.body.appendChild(circle);
+
+
+  let mouseX = 0, mouseY = 0;
+  let circleX = 0, circleY = 0;
+  const circleSpeed = 0.02; 
+
+
+  document.addEventListener("mousemove", (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+  });
+
+
+  function animateCircle() {
+    circleX += (mouseX + 10 - circleX) * circleSpeed;
+    circleY += (mouseY - 50 - circleY) * circleSpeed;
+    circle.style.transform = `translate(${circleX}px, ${circleY}px)`;
+    requestAnimationFrame(animateCircle);
+  }
+
+
+  animateCircle();
+
+  // circle ____________________________________________END
+  
+
+
+  setTimeout(() => {
+    welcomeText1.style.animation = "fadeOutAndUp 1.5s forwards";
+
+    setTimeout(() => {
+      welcomeText1.classList.add("hidden");
+      welcomeText2.classList.remove("hidden");
+      welcomeText2.style.animation = "fadeIn 0.5s forwards";
+    }, 500); //time (ms)
+  }, 1500); //time (ms)
+
+
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      
+      const targetId = button.getAttribute("data-target");
+      const targetFrame = document.getElementById(targetId);
+
+      const targetBtnFrame = document.getElementById(button.getAttribute("id"));
+
+      frames.forEach((frame) => frame.classList.remove("active"));
+      targetFrame.classList.add("active");
+
+      buttons.forEach((btn) => btn.classList.remove("active"));
+      targetBtnFrame.classList.add("active");
+
+      if (targetId === "welcome") {
+        isTypingAboutMe = false
+
+        welcomeText1.classList.remove("hidden");
+        welcomeText2.classList.add("hidden");
+
+        welcomeText1.style.animation = "none";
+        welcomeText2.style.animation = "none";
+
+        setTimeout(() => {
+          welcomeText1.style.animation = "fadeOutAndUp 1.5s forwards";
+
+          setTimeout(() => {
+            welcomeText1.classList.add("hidden");
+            welcomeText2.classList.remove("hidden");
+            welcomeText2.style.animation = "fadeIn 0.5s forwards"; 
+          }, 500); //time (ms)
+
+        }, 1500); //time (ms)
+      };
+
+      if (targetId === "about-me") {
+        isTypingAboutMe = true
+        document.getElementById("typing-text").innerHTML = '';
+        const textAboutMe = 'My name is Artur. \n I am a very sociable and friendly person. \n I like programming and UI/UX Design.';
+        const speedTextAboutMe = 50; // speedTime (мс)
+        let index = 0;
+        function typeText() {
+          if (index < textAboutMe.length && isTypingAboutMe) {
+            const currentChar = textAboutMe[index];
+            if (currentChar === "\n") {
+              document.getElementById("typing-text").innerHTML += "<br>";
+            } else {
+              document.getElementById("typing-text").innerHTML += currentChar;
+            }
+
+            index++;
+            setTimeout(typeText, speedTextAboutMe);
+          }
+        }
+
+        typeText();
+      };
+
+      if (targetId === "stack") {
+        isTypingAboutMe = false
+        const isMobile = window.matchMedia("(pointer: coarse)").matches;
+
+        if (!isMobile) {
+          cards.forEach((card) => {
+            card.addEventListener('mousemove', (e) => {
+              const rect = card.getBoundingClientRect();
+              const x = e.clientX - rect.left;
+              const y = e.clientY - rect.top;
+            
+              const centerX = rect.width / 2;
+              const centerY = rect.height / 2;
+            
+              const rotateX = (y - centerY) / 7;
+              const rotateY = (x - centerX) / 7;
+            
+              const shadowX = (x - centerX) / 7;
+              const shadowY = (y - centerY) / 7;
+            
+              requestAnimationFrame(() => {
+                card.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+                card.style.boxShadow = `${-shadowX}px ${-shadowY}px 1em rgba(0, 0, 0, 0.3)`;
+              });
+            });
+          
+            card.addEventListener('mouseleave', () => {
+              requestAnimationFrame(() => {
+                card.style.transform = 'rotateX(0) rotateY(0) scale(1)';
+                card.style.boxShadow = '0 0em 0em rgba(0, 0, 0, 0)';
+              });
+            });
+          });
+        };
+      };
+
+      if (targetId === "projects") {
+        isTypingAboutMe = false
+      };
+      
+      if (targetId === "contacts") {
+        isTypingAboutMe = false
+      }
+    })
+  });
+});
